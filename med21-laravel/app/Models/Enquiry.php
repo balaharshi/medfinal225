01e9cba51e415bd76991fd0230ac5cb8f7362f79
Varun Routhu <varunrouthu@gmail.com>
2026-07-16 10:57:30 +0400

    fix: add id to Enquiry fillable array

--- med21-laravel/app/Models/Enquiry.php
+++ med21-laravel/app/Models/Enquiry.php
@@ -5,7 +5,7 @@ namespace App\Models;
 class Enquiry extends BaseModel
 {
     protected $fillable = [
-        'customer_name', 'customer_email', 'customer_phone', 'service_title',
+        'id', 'customer_name', 'customer_email', 'customer_phone', 'service_title',
         'message', 'contact_method', 'date', 'status',
     ];
 }
